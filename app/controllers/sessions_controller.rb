e851574f524ec9100ae3ce88803ef141db27be9c
isaacmabueno <isaacmabueno@gmail.com>
2015-08-16 22:55:27 -0500

    working sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,24 @@ class SessionsController < ApplicationController
 #post
   def confirmation
     #show a user message reguarding login status
+    #this is the instance variable of our current user
+    # puts '------users#confirmation-------'
+    # puts puts
+    # puts '-----end users#confirmation-------'
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      @message = 'User account or password are valid'
+      render '/sessions/login'
   end
+end
 #get
   def logout
     # logout
+    session[:user_id] = nil
+    redirect_to '/users/login'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,34 @@ class UsersController < ApplicationController
   #get
   def signup
     #render a view ad let a user register
+    #my only job is to render a form...yayyyy i have a job!
+
+
+
   end
 #post
   def confirmation
+    # puts '------users#confirmation-------'
+    # puts user_params
+    # puts '-----end users#confirmation-------'
     #post request that handles information/says thanks
+    @user = User.new(user_params)
+    if @user.save
+      #thanks, youre in!
+      session[:user_id] = @user.id
+
+      # Destination.where(:user.id => sessions[:user_id].to_i)
+    else
+      @message = 'User account extists or passwords do not match'
+      render 'users/signup'
+      #womp womp...error message
+  end
+end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
